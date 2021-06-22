32a6ec4105065c7cea948968d8279fba03209e39
MuhammadWiman <73020035+MuhammadWiman@users.noreply.github.com>
2021-06-22 11:42:46 +0700

    Update Home-Automation-Input.ino

--- Microcontroller/Home-Automation-Input.ino
+++ Microcontroller/Home-Automation-Input.ino
@@ -1,8 +1,8 @@
 /*
     Develop by      : Fahmi Nurfadilah 
     Email           : fahmi.nurfadilah1412@gmail.com
-    Project         : IoT General
-    Version         : 1.2
+    Project         : LSKK HomeAutomation
+    Version         : 2.1
 */
 
 #include <PubSubClient.h>
@@ -10,33 +10,30 @@
 
 // Update these with values suitable for your network.
 
-const char* ssid = "LSKK_Lantai2"; /// ssid/nama wifi yang telah terkoneksi internet
-const char* password = "lskk12345"; //paassword wifi
-const char* mqtt_server = "167.205.7.19";// mqtt server
-const char* mqtt_user = "/workshopmitra:workshopmitra"; //user mqtt
-const char* mqtt_pass = "passwordnyaqwerty";//password mqtt
-const char* mqtt_pub_topic = "Sensor";//antrian data input
-const char* CL = "LSKK-HA-Saklar-1";//nama alat
+const char* ssid = "LSKKHomeauto";//
+const char* password = "1234567890";
+const char* mqtt_server = "192.168.0.2";//ip server
+const char* mqtt_user = "/Homeauto:homeauto";
+const char* mqtt_pass = "homeauto12345!";
+const char* mqtt_pub_topic = "Sensor";//publish topic pada mqtt
+const char* CL = "LSKK-HA-";//nama alat
+int loop_count  = 0 ; //loop count loop
 
-String statusDevice[4] = {"1", "1", "1", "1"};
+String statusDevice [4] = {"1","1","1","1"};
 
-int input1 = D1 ;
-int input2 = D2 ;
-int input3 = D3 ;
-int input4 = D4 ;
+const int input1 = D1;
+const int input2 = D2;
+const int input2 = D3;
+const int input2 = D4;
 
 
-int instate1 = 0;
-int instate2 = 0;
-int instate3 = 0;
-int instate4 = 0;
+
 
 
 int prevState1 = 0;
 int prevState2 = 0;
 int prevState3 = 0;
 int prevState4 = 0;
-
 
 char message [100];
 
@@ -113,124 +110,66 @@ void reconnect() {
   }
 }
 
+void watchdogSetup(void) {
+  ESP.wdtDisable();
+}
+
 void setup()
 {
   Serial.begin(115200);
-  //pinMode(input, INPUT);
   pinMode(input1, INPUT_PULLUP);
   pinMode(input2, INPUT_PULLUP);
-  pinMode(input3, INPUT_PULLUP);
-  pinMode(input4, INPUT_PULLUP);
+
+
   setup_wifi();
   printMACAddress();
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
+  watchdogSetup();
 }
+
+String button_status[4];
+String prevpubmsg = "";
 void loop() {
   String pubmsg = "";
-  String value = String(statusDevice[0] + statusDevice[1] + statusDevice[2] + statusDevice[3]);
-  String input_guid = "5183a824-3609-11eb-adc1-0242ac120002";
-  String input_value = value;
-  int callstate[4] = {instate1,instate2,instate3,instate4};
-  if (!client.connected()) {
-    reconnect();
+  String input_guid = "2bf0c806-eebb-467c-9a47-cff1ded5263c";//guid id perangkat
+  for (int i = 0; i <= loop_count; i++) {
+    if (!client.connected()) {
+      reconnect();
+    }
+    client.loop();
   }
-  client.loop();
-  instate1 = digitalRead(input1);
-  instate2 = digitalRead(input2);
-  instate3 = digitalRead(input3);
-  instate4 = digitalRead(input4);
+  loop_count++;
+  ESP.wdtFeed();
+  Serial.print(loop_count);
+  Serial.print(". Watchdog fed in approx. ");
+  Serial.print(loop_count * 1000);
+  Serial.println(" milliseconds.");
+  delay(1000);
+    
+  int instate1 = digitalRead(input1);
+  if(instate1==LOW) button_status[0]="0";
+  else button_status[0]="1";
+
+  int instate2 = digitalRead(input2);
+  if(instate2==LOW) button_status[1]="0";
+  else button_status[1]="1";
+
+  int instate3 = digitalRead(input3);
+  if(instate3==LOW) button_status[2]="0";
+  else button_status[2]="1";
+
+  int instate4 = digitalRead(input4);
+  if(instate4==LOW) button_status[3]="0";
+  else button_status[3]="1";
   
-  //Serial.print("Publish message: ");
-  //pubmsg = input_guid + "#" + input_value;
-  //Serial.println(pubmsg);
-  //client.publish(mqtt_pub_topic, pubmsg.c_str());
-  if (instate1 == 1) {
-    if(prevState1 != instate1){
-      statusDevice[0] = "0";
-      Serial.println("Input detected!");
-      pubmsg = input_guid + "#" + input_value;
-      Serial.println(pubmsg);
-      client.publish(mqtt_pub_topic, pubmsg.c_str());
-      Serial.println("Success send to RMQ");
-      prevState1 = callstate[0];
-    }
+
+  
+  pubmsg = input_guid + "#" + button_status[0] + button_status[1]+ button_status[2]+ button_status[3];
+  //Kirim
+  if(pubmsg != prevpubmsg) {
+    client.publish(mqtt_pub_topic, pubmsg.c_str());
+    Serial.println(pubmsg);
+    prevpubmsg=pubmsg;
   }
-  if (instate1 == 0) {
-    if(prevState1 != instate1){
-      statusDevice[0] = "1";
-      Serial.println("Input not detected!");
-      pubmsg = input_guid + "#" + input_value;
-      Serial.println(pubmsg);
-      client.publish(mqtt_pub_topic, pubmsg.c_str());
-      Serial.println("Success send to RMQ");
-      prevState1 = callstate[0];
-  }
-}
-if (instate2 == 1) {
-    if(prevState2 != instate2){
-      statusDevice[1] = "0";
-      Serial.println("Input detected!");
-      pubmsg = input_guid + "#" + input_value;
-      Serial.println(pubmsg);
-      client.publish(mqtt_pub_topic, pubmsg.c_str());
-      Serial.println("Success send to RMQ");
-      prevState2 = callstate[1];
-    }
-  }
-  if (instate2 == 0) {
-    if(prevState2 != instate2){
-      statusDevice[1] = "1";
-      Serial.println("Input not detected!");
-      pubmsg = input_guid + "#" + input_value;
-      Serial.println(pubmsg);
-      client.publish(mqtt_pub_topic, pubmsg.c_str());
-      Serial.println("Success send to RMQ");
-      prevState2 = callstate[1];
-  }
-}
-if (instate3 == 1) {
-    if(prevState3 != instate3){
-      statusDevice[2] = "0";
-      Serial.println("Input detected!");
-      pubmsg = input_guid + "#" + input_value;
-      Serial.println(pubmsg);
-      client.publish(mqtt_pub_topic, pubmsg.c_str());
-      Serial.println("Success send to RMQ");
-      prevState3 = callstate[2];
-    }
-  }
-  if (instate3 == 0) {
-    if(prevState3 != instate3){
-      statusDevice[2] = "1";
-      Serial.println("Input not detected!");
-      pubmsg = input_guid + "#" + input_value;
-      Serial.println(pubmsg);
-      client.publish(mqtt_pub_topic, pubmsg.c_str());
-      Serial.println("Success send to RMQ");
-      prevState3 = callstate[2];
-  }
-}
-if (instate4 == 1) {
-    if(prevState4 != instate4){
-      statusDevice[3] = "0";
-      Serial.println("Input detected!");
-      pubmsg = input_guid + "#" + input_value;
-      Serial.println(pubmsg);
-      client.publish(mqtt_pub_topic, pubmsg.c_str());
-      Serial.println("Success send to RMQ");
-      prevState4 = callstate[3];
-    }
-  }
-  if (instate4 == 0) {
-    if(prevState4 != instate4){
-      statusDevice[3] = "1";
-      Serial.println("Input not detected!");
-      pubmsg = input_guid + "#" + input_value;
-      Serial.println(pubmsg);
-      client.publish(mqtt_pub_topic, pubmsg.c_str());
-      Serial.println("Success send to RMQ");
-      prevState4 = callstate[3];
-  }
-}
 }
